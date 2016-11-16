1660537bb9d7e28115645907ef97ebb7644e97b7
Chip C <chipcracraft@gmail.com>
2016-11-16 18:03:56 -0500

    changes to signup

--- src-client/scripts/app.js
+++ src-client/scripts/app.js
@@ -2,6 +2,5 @@ const ReactDOM = require('react-dom');
 const React = require('react')
 const Backbone = require('backbone');
 
-document.querySelector('#app-container').innerHTML = `<h1>Yah okay</h1>`
 
 new AppRouter()
--- src-client/scripts/stores.js
+++ src-client/scripts/stores.js
@@ -3,6 +3,27 @@ const React = require('react')
 const Backbone = require('backbone');
 
 const STORE = {
+    _data : {
 
-  
+      currentViewSetting: '',
+      currentSongs: [],
+    },
+
+    setStore: function(storeProp, payload){
+
+    }
+    this._data[storeProp] = payload
+    BackBone.Events.trigger('storeChange')
+},
+
+    getStoreData: function(){
+       return this._data
+    },
+
+    onChange: function(changeStore){
+        Backbone.Events.on('storeChange', someFunc)
+
+    }
 }
+
+modules.export = STORE
