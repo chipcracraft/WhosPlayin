6379e801a21a66f9f327f28e3a3857d7a31b2958
William McCullough <williammccullough@Williams-MacBook-Air.local>
2016-11-18 15:51:37 -0500

    capturing user obj

--- src-client/scripts/app.js
+++ src-client/scripts/app.js
@@ -1,3 +1,1 @@
-const ReactDOM = require('react-dom');
-const React = require('react')
-const Backbone = require('backbone');
+
--- src-client/scripts/sign-up-component.js
+++ src-client/scripts/sign-up-component.js
@@ -3,30 +3,57 @@ const ReactDOM = require('react-dom')
 
 
 const SignUpView = React.createClass({
+      _handleSignIn: function(evt){
+         evt.preventDefault()
 
-        render: function(){
-            return (
-                  <div className="login-page">
-                    <div>
-                      <form action="" className="sign-in">
-                          <h1>Sign In</h1>
-                          <input type="Email Address"/>
-                          <input type="Password"/>
-                          <button>Sign In</button>
-                      </form>
-                    </div>
-                    <form action="" className="sign-up">
-                        <h1>Sign Up</h1>
-                        <input type="First Name"/>
-                        <input type="Last Name"/>
-                        <input type="Email"/>
-                        <input type="Password"/>
-                        <button>Sing Up</button>
-                    </form>
-                  </div>
+         let userObj = {
+            name: evt.target.username.value,
+            password: evt.target.password.value
+         }
 
-            )
-        }
+         console.log(userObj)
+      },
+
+      _handleSignUp: function(evt){
+         evt.preventDefault()
+
+         let newUserObj = {
+            firstName: evt.target.firstName.value,
+            lastName: evt.target.lastName.value,
+            email: evt.target.email.value,
+            username: evt.target.username.value,
+            password: evt.target.password.value
+         }
+
+         console.log(newUserObj)
+      },
+
+      render: function(){
+         return (
+               <div className="login-page">
+                 <div>
+                   <form action="" className="sign-in" onSubmit={this._handleSignIn}>
+                       <h2>Sign In</h2>
+                       <input type="text" name="username"/>
+                       <input type="Password" name="password"/>
+                       <input type="submit" value="Sign In" />
+                   </form>
+                 </div>
+                 <form action="" className="sign-up" onSubmit={this._handleSignUp}>
+                     <h2>Sign Up</h2>
+                     <input type="First Name" name="firstName"/>
+                     <input type="Last Name" name="lastName"/>
+                     <input type="Email" name="email"/>
+                     <input type="username" name="username"/>
+                     <input type="Password" name="password"/>
+                     <input type="submit" value="Sign Up" />
+                 </form>
+               </div>
+
+                 )
+       }
 })
 
 ReactDOM.render(<SignUpView/>, document.querySelector('#app-container'))
+
+module.exports=SignUpView
