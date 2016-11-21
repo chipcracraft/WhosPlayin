73876b3fe4a24817c91bead1041ef7409e5ff6c0
Chip C <chipcracraft@gmail.com>
2016-11-21 09:41:10 -0500

    band-view and appview

---
+++ src-client/scripts/app-view-controller.js
@@ -0,0 +1,9 @@
+const React = require('react');
+const STORE = require('./store,js');
+const ACTIONS = require('./actions.js');
+
+const AppViewController = React.createClass({
+
+
+  
+})
---
+++ src-client/scripts/band-view.js
@@ -0,0 +1,11 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+
+const BandView = React.createClass ({
+
+
+
+
+  
+})
