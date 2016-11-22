d145aba05661ba1d7000418e0c1b0985d7c8f34d
J <jordanmledford@gmail.com>
2016-11-22 14:42:50 -0500

    changed home-view yet again, updated classNames in sign-up component

--- src-client/scripts/main-view.js
+++ src-client/scripts/main-view.js
@@ -23,6 +23,15 @@ const hamburgerStyle = {
   top: 10,
 }
 
+const schema = {
+  bandName:
+  trackName:
+  dateTime:
+  trackSet:
+  /*trackset is for a spotify playlist populated
+  by unique trackIDs separated by semicolons*/
+}
+
 const HomeView = React.createClass({
   render: function(){
     return (
@@ -50,9 +59,9 @@ const HomeView = React.createClass({
                   </div>
                   <div className="col l4 s10">
                     <ul>
-                      <li><a className="grey-text text-lighten-3 right" href="#!">this.props.band-name</a></li>
+                      <li><a className="grey-text text-lighten-3 right" href="#!">{this.props.bandname}</a></li>
                       <br></br>
-                      <li><a className="grey-text text-lighten-3 right" href="#!">this.props.artist-name</a></li>
+                      <li><a className="grey-text text-lighten-3 right" href="#!">{this.props.artistname}</a></li>
                     </ul>
                   </div>
                 </div>
@@ -65,24 +74,23 @@ const HomeView = React.createClass({
 const CardView = React.createClass({
   render: function(){
     return (
-        <div className="col s12 m3">
+        <div className="col s12 m4 hoverable">
           <div className="card small z-depth-5">
             <div className="card-content black-text">
               <div className="card-image">
                 <img src="https://unsplash.it/200/?random"></img>
               </div>
-              <span className="card-title">this.props.band-name</span>
-              <h6>this.props.artist-name.bio</h6>
-              <h6>this.props.date-time</h6>
-
+              <span className="card-title">{this.props.bandname}</span>
+              <h6>{this.props.artistname}</h6>
+              <h6>{this.props.datetime}</h6>
             </div>
-            <div className="card-action">
-              <iframe src="https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:5Z7ygHQo02SUrFmcgpwsKW,1x6ACsKV4UdWS2FMuPFUiT,4bi73jCM02fMpkI11Lqmfe" frameBorder="0" allowTransparency="true"></iframe>
-              <a href="/view-more">view more</a>
+            <div className="cta">
+              <iframe src="https://embed.spotify.com/?uri=spotify:trackset:WOWILOVEMEMES:4uLU6hMCjMI75M1A2tKUQC,1x6ACsKV4UdWS2FMuPFUiT,4bi73jCM02fMpkI11Lqmfe" frameBorder="0" allowTransparency="true"></iframe>
+              <a href="/band-view">view full playlist</a>
             </div>
           </div>
         </div>
-    )
+    );
   }
 });
 // let muiName = ''
