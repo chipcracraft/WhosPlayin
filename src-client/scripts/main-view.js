0879329e9ce4b51559527d38fcdfbff57b25a13c
J <jordanmledford@gmail.com>
2016-11-26 02:52:26 -0500

    forgot to commit

--- src-client/scripts/main-view.js
+++ src-client/scripts/main-view.js
@@ -40,7 +40,6 @@ const HomeView = React.createClass({
   render: function(){
     return (
             <div className="main-container" style={mainStyle}>
-              <a href="/login">log in</a>
               {/* <Login /> */}
               {/* <i className="fa fa-bars fa-4x hamburger" aria-hidden="true" style={hamburgerStyle}></i> */}
               <div className="row center">
@@ -78,7 +77,7 @@ const HomeView = React.createClass({
 const CardView = React.createClass({
   render: function(){
     return (
-        <div className="col s12 m4">
+        <div className="col m4">
           <div className="card">
             <div className="card-content black-text">
               <div className="card-image">
@@ -89,8 +88,9 @@ const CardView = React.createClass({
               <h6>{this.props.datetime}</h6>
             </div>
             <div className="cta">
+              {/* <iframe src="https://embed.spotify.com/?uri=spotify:playlist:788MOXyTfcUb1tdw4oC7KJ" width="300" height="380" frameborder="0" allowtransparency="true"></iframe> */}
               <iframe src="https://embed.spotify.com/?uri=spotify:trackset:WOWTESTTESTTEST:4uLU6hMCjMI75M1A2tKUQC,3LX0Qc1iKkqLZP0FijjLmI,1x6ACsKV4UdWS2FMuPFUiT,4bi73jCM02fMpkI11Lqmfe" frameBorder="0" allowTransparency="true"></iframe>
-              <a href="/band-view">view full playlist</a>
+              <a href="#bandinfo">view full playlist</a>
             </div>
           </div>
         </div>
