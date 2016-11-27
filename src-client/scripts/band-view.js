08bcc3f62d9afe0fb7333450254390708bb9b93c
J <jordanmledford@gmail.com>
2016-11-27 17:30:49 -0500

    i hope this works chip

--- src-client/scripts/band-view.js
+++ src-client/scripts/band-view.js
@@ -11,18 +11,17 @@ const BandView = React.createClass ({
           render: function(){
               return (
                   <div className="container band-info">
-                    <h1 className="band-title">Interpol <span>Turn On The Bright Lights</span></h1>
+                    <h1 className="band-title">Interpol<span>Turn On The Bright Lights</span></h1>
                     <div className="row">
-                      <div className="left-column col-xs-12 col-sm-6">
+                      <div className="left-column col sm6 md6 lg6">
                         <div className="band-image">
                           <img className ="band-img" src="https://lastfm-img2.akamaized.net/i/u/ar0/55292eb15e7e442a9f40f09625d6e111"/>
                           <p className="band-descript">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
                         </div>
                       </div>
-                      <div className="right-column col-xs-12 col-sm-6">
+                      <div className="right-column col sm6 md6 lg6">
                         <iframe src="https://embed.spotify.com/?uri=spotify:user:erebore:playlist:788MOXyTfcUb1tdw4oC7KJ&theme=white" width="500" height="640" frameBorder="0" allowTransparency="true"></iframe>
                       </div>
-
                     </div>
                   </div>
               )
--- src-client/scripts/new-main.js
+++ src-client/scripts/new-main.js
@@ -7,7 +7,6 @@ const MainView =  React.createClass({
     return (
       <div className="wrapper">
         <div className="hero">
-          <h1>wow hopefully this will work</h1>
           <ul id="dropdown1" className="dropdown-content">
             <li><a href="#!">one</a></li>
             <li><a href="#!">two</a></li>
@@ -16,10 +15,10 @@ const MainView =  React.createClass({
           </ul>
           <nav>
             <div className="nav-wrapper">
-              <a href="#!" className="brand-logo">Logo</a>
+              <a href="#!" className="brand-logo">whosplayin</a>
               <ul className="right hide-on-med-and-down">
-                <li><a href="sass.html">Sass</a></li>
-                <li><a href="badges.html">Components</a></li>
+                <li><a href="/login">Login</a></li>
+                <li><a href="#ok">home</a></li>
                 <li><a className="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i className="fa fa-chevron-down" aria-hidden="true"></i></a></li>
               </ul>
             </div>
@@ -47,7 +46,7 @@ const CardView = React.createClass({
             </div>
             <div className="cta">
               {/* <iframe src="https://embed.spotify.com/?uri=spotify:playlist:788MOXyTfcUb1tdw4oC7KJ" width="300" height="380" frameborder="0" allowtransparency="true"></iframe> */}
-              <iframe src="https://embed.spotify.com/?uri=spotify:trackset:WOWTESTTESTTEST:4uLU6hMCjMI75M1A2tKUQC,3LX0Qc1iKkqLZP0FijjLmI,1x6ACsKV4UdWS2FMuPFUiT,4bi73jCM02fMpkI11Lqmfe" frameBorder="0" allowTransparency="true"></iframe>
+              <iframe src="https://embed.spotify.com/?uri=spotify:trackset:zombyisadouche:51265tsBntG5gAcOBMvBeD,4uLU6hMCjMI75M1A2tKUQC,3LX0Qc1iKkqLZP0FijjLmI,1x6ACsKV4UdWS2FMuPFUiT,4bi73jCM02fMpkI11Lqmfe" frameBorder="0" allowTransparency="true"></iframe>
               <a href="#bandinfo">view full playlist</a>
             </div>
           </div>
