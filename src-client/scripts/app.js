d144c87c25f033a0a455e61bb0cde738e64d9360
J <jordanmledford@gmail.com>
2016-11-28 17:50:06 -0500

    loggin data in order

--- src-client/scripts/app.js
+++ src-client/scripts/app.js
@@ -1,7 +1,6 @@
 const ReactDOM = require('react-dom');
 const React = require('react');
 const Backbone = require('backbone');
-const {fetchMetroData, fetchMetroConcerts, userMetro, metroConcerts} = require('./node-queries.js')
 const SignUpView = require('./sign-up-component.js');
 const SplashPageView = require('./component-splash.js');
 const BandView = require('./band-view.js');
--- src-client/scripts/new-main.js
+++ src-client/scripts/new-main.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {
-  fetchBackEnd,
-  fetchArtists,
-  fetchTopTracks
-} from './test.js'
+import {fetchBackEnd,
+        fetchArtists,
+        fetchTopTracks } from './test.js'
 
 const MainView = React.createClass({
   componentWillMount: function() {
-    let stuff = fetchBackEnd('Asheville')
-    stuff.then(function(res){
+    let locationDataReq = fetchBackEnd('Asheville')
+    locationDataReq.then(function(res){
       console.log('res', res);
-    });
+
+      let artistListReq = fetchArtists();
+      artistListReq.then(function(res){
+        console.log('res2', res);
+
+      })
+    })
+    // let eventsListNames = performance[0]
+    // console.log(eventsListNames);
+
   },
   render: function(){
     return (
