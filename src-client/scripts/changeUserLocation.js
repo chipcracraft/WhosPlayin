040f05c2d1c84516196f3fbb2f3f1d549fe8b99c
William McCullough <williammccullough@Williams-Air.attlocal.net>
2016-11-29 23:31:43 -0500

    changelocation done

--- src-client/scripts/changeUserLocation.js
+++ src-client/scripts/changeUserLocation.js
@@ -18,13 +18,21 @@ _changeUserLocation: function(userNewLocationObj){
 },
 
 
-// NEW BUTTON IN MAIN NAVBAR & CLICKHANDLER
+// NEW BUTTON IN MAIN  & CLICKHANDLER
 
-<li onClick={this._changeLocationClickHandler}><a>Change Location</a></li>
+<form className="sign-in" onSubmit={this._changeLocationSubmitHandler}>
+   <input type="text" value="Location" name="city"></input>
+   <button type="submit" className="center location-btn btn waves-effect waves-light">Change Location</button>
+</form>
 
 
-_changeLocationClickHandler = function(){
+
+_changeLocationSubmitHandler = function(){
    evt.preventDefault()
 
-   
+   let userNewLocationObj = {
+      city: evt.target.city.value,
+   }
+
+   ACTIONS._changeUserLocation(userNewLocationObj)
 },
